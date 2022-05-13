1ad37fc7951835e0fc43565a12534e8f28580a89
eric-chambonneau <eric.chambonneau@gmail.com>
2022-05-13 22:05:31 +0200

    1init

---
+++ index.js
@@ -0,0 +1,10 @@
+// SELECTEURS
+//document.querySelector('h4').style.background = "yellow";
+//const baliseHTML = document.querySelector("h4");
+
+// Click event
+const questionContainer = document.querySelector(".click-event");
+
+questionContainer.addEventListener("click" , () => {
+  questionContainer.style.background = "red";
+});
